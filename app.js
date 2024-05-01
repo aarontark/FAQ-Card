9ed956ff7e7602889ceee634e59169a44dc56c23
aarontark <tarkington.aaron@gmail.com>
2024-04-30 21:38:27 -0500

    made each question expandable

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const questionSection = document.querySelectorAll('.question-section');
 const answer = document.querySelectorAll('.answer');
 const expandIcon = document.querySelectorAll('.expand');
+const titleSection = document.querySelectorAll('.title-section');
 
-const checkExpand = () => {
-    for (question of questionSection) {
-        if (question.classList.contains('expanded')) {
-            return true;
+
+
+for (const q of questionSection) {
+    q.addEventListener('click', () => {
+        const expandedAdd = () => {
+            q.classList.add('expanded');
+            q.children[1].classList.add('answer-expanded')
+            q.children[1].classList.remove('answer');
+            q.querySelector('.expand').src = "./assets/images/icon-minus.svg";
         }
-    }
-}
 
+        const expandedRemove = () => {
+            q.classList.remove('expanded');
+            q.children[1].classList.remove('answer-expanded')
+            q.children[1].classList.add('answer');
+            q.querySelector('.expand').src = "./assets/images/icon-plus.svg";
+        }
+
+
+        if (q.classList.contains('expanded')) {
+            expandedRemove();
+        } else {
+            expandedAdd();
+        }
+})}
